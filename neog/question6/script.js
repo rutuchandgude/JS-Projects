979e8880a24f7f52675e27cb6e0386fd21297956
Rutuja Chandgude <74643325+rutuchandgude@users.noreply.github.com>
2021-12-18 12:54:56 +0530

    Delete neog directory

--- neog/question6/script.js
+++
@@ -1,9 +0,0 @@
-const input=document.querySelector("#input");
-const button=document.querySelector("#btn");
-const output=document.querySelector("output");
-
-button.addEventListener('click',displayOp)
-
-function displayOp(){
-  input.style.display="none";
-}
